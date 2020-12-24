d74a4251da8c0c2dac2f5e4251e91f288f732d43
Rain120 <chao1202016@outlook.com>
2020-12-24 21:21:58 +0800

    feat: docker repository version

--- scripts/build-images.js
+++ scripts/build-images.js
@@ -1,13 +1,33 @@
 const {exec} = require('child_process');
+const { version, author } = require('../package.json');
 
-console.log("------------ docker build images start ------------\n");
+const isLocal = process.argv[2] === 'local';
+
+console.log(`------------ ${isLocal ? 'Local' : 'Remote'} Docker Build Images Start ------------\n`);
 
 // const run = 'docker run -d --name qq-music-api -p 3200:3200 qq-music-api'
-const cmd = 'docker build -t qq-music-api .';
+const image = `${isLocal ? '' : `${author.toLowerCase()}/`}qq-music-api:${version}`
+const cmd = isLocal
+	? `docker build -t ${image} .`
+	: `docker image tag qq-music-api:${version} ${image}`;
+
+console.log('cmd', cmd, '\n');
 exec(cmd, {}, (err, stdout, stderr) => {
 	if (err) {
 		console.log(err);
 	}
 	console.log(stdout, stderr);
-	console.log("------------ docker build images end ------------\n");
+	console.log(`------------ ${isLocal ? 'Local' : 'Remote'} Docker Build Images End ------------\n`);
+	if (!isLocal) {
+		const pushCmd = `docker image push ${image}`;
+		console.log('pushCmd', pushCmd, '\n');
+		console.log(`------------ ${isLocal ? 'Local' : 'Remote'} Docker Push Images Start ------------\n`);
+		exec(pushCmd, {}, (err, stdout, stderr) => {
+			if (err) {
+				console.log(err);
+			}
+			console.log(stdout, stderr);
+			console.log(`------------ ${isLocal ? 'Local' : 'Remote'} Docker Push Images End ------------\n`);
+		});
+	}
 });
