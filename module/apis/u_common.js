6439cc97d5916fb5dcfb95b4fc7147576b6538b0
Ctaoist <liao9256@163.com>
2024-09-02 11:35:52 +0800

    fix: 修复设置 cookie 也不能获取 vip 歌曲下载链接 #91 #49

--- module/apis/u_common.js
+++ module/apis/u_common.js
@@ -1,5 +1,6 @@
 const request = require('../../util/request');
 const config = require('../config');
+const userInfoConfig = require('../../config/user-info');
 
 module.exports = ({ options = {}, method = 'get' }) => {
 	let opts = Object.assign(options, config.commonParams, {
@@ -7,8 +8,9 @@ module.exports = ({ options = {}, method = 'get' }) => {
 			referer: 'https://y.qq.com/portal/player.html',
 			host: 'u.y.qq.com',
 			'content-type': 'application/x-www-form-urlencoded',
+			'cookie': userInfoConfig.cookie,
 		},
 	});
-	console.log('https://u.y.qq.com/cgi-bin/musicu.fcg', {opts})
+	console.log('https://u.y.qq.com/cgi-bin/musicu.fcg', { opts })
 	return request('https://u.y.qq.com/cgi-bin/musicu.fcg', method, opts, 'u');
 };
